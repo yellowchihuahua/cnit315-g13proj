29669bda93bb178509b3a83ebb1423e0ff56a233
yellowchihuahua <47878958+yellowchihuahua@users.noreply.github.com>
2023-10-28 14:58:23 -0400

    adjust ll

--- main.c
+++ main.c
@@ -1,8 +1,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "linkedlist.h"
 #include <curl/curl.h>
+
+struct Node {
+    char *originalUrl;
+    char *shortenedUrl;
+    struct Node *next;
+};
+
+
+struct LinkedList{
+	Node* head;
+};
+
+struct Node* CreateNode(char *originalUrl, char *shortenedUrl){
+	Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+	if (newNode == NULL) {
+		printf("CreateNode(char *originalUrl, char *shortenedUrl); -- Failed memory allocation.\n");
+		return NULL;
+	}
+
+	strcpy(newNode->originalUrl, originalUrl);
+	strcpy(newNode->shortenedUrl, shortenedUrl);
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+struct LinkedList* CreateList() {
+	LinkedList* llist = malloc(sizeof(struct LinkedList));
+	if (llist == NULL) {
+		printf("CreateList(); -- Failed memory allocation.\n");
+		return NULL;
+	}
+	llist->head = NULL;
+	return llist;
+}
+
+int GetListLength(struct LinkedList* llist){
+	int count = 0;
+	struct Node* head = llist->head;
+	if (head == NULL){
+		return count;
+	}
+	count++;
+	struct Node* current = head;
+	while(current->next != NULL){
+		current = current->next;
+		count++;
+	}
+
+	return count;
+}
+
+int ToCsv(struct Node* node, char *string) {
+	if(node == NULL){
+		printf("ToCsv(struct Node* node, char *string); -- Node is null, nothing to convert.\n");
+		return 0;
+	}
+	
+	snprintf(string, 256, "%s, %s\n", node->originalUrl, node->shortenedUrl);
+	return 1;
+}
+
+int ToString(struct Node* node, char *string) {
+	if(node == NULL){
+		printf("ToCsv(struct Node* node, char *string); -- Node is null, nothing to convert.\n");
+		return 0;
+	}
+	snprintf(string, 256, "Data: %s, %s\n", node->originalUrl, node->shortenedUrl);
+	return 1;
+}
+
+void PrintNode(struct Node* node){
+	if (node == NULL) {
+		printf("PrintNode(struct Node* node); -- Given node is NULL, nothing to print.\n");
+		return;
+	}
+	char nodeStr[256];
+	ToString(node, nodeStr);
+	printf("%s", nodeStr);
+}
+
+void PrintList(struct LinkedList* llist){
+	if(GetListLength(llist) == 0){
+		printf("PrintList(struct LinkedList* llist); -- Empty list, nothing to print.\n");
+		return;
+	}
+	struct Node* current = llist->head;
+	while (current != NULL) {
+		PrintNode(current);
+		current = current->next;
+	}
+}
+
+void PrintSearch(struct LinkedList* llist, char* searchRequest) {
+	struct Node* current = llist->head;
+	char nodeString[256];
+
+	while(current != NULL) {
+		ToCsv(current, nodeString);
+		if(strstr(nodeString, searchRequest) != NULL){
+			PrintNode(current);
+		}
+		current = current->next;
+	}
+}
+
+void FreeList(struct LinkedList* llist){
+//traverse through and free allocated memory
+	struct Node* temp = llist->head;
+    while ((llist->head) != NULL) {
+        temp = (llist->head);
+		(llist->head) = (llist->head)->next;
+		free(temp);
+    }
+	free(llist);
+}
+
+int InsertFront(struct LinkedList* llist, struct Node* nodeToInsert){
+	if(nodeToInsert == NULL) {
+		printf("InsertFront(struct LinkedList* llist, struct Node* nodeToInsert); -- Node is null, nothing to insert.\n");
+		return 0;
+	}
+	if (GetListLength(llist) == 0){
+		llist->head = nodeToInsert;
+		return 1;
+	}
+	nodeToInsert->next = llist->head; //points next var to front of list, global variable
+	llist->head = nodeToInsert; //inserts node and sets pointer of new front, global varaible
+	return 1;
+}
+
 
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, char**userp){
     size_t totalSize = size*nmemb;
@@ -56,9 +186,10 @@ void DisplayMenu(){
         printf("Here are your options: \n");
         printf("(1) Enter a URL to shorten\n");
         printf("(2) Show history of URLs you have shortened while using this program\n");
-        printf("(3) Credits\n");
-        printf("(4) Exit the program\n");
-        printf("\nEnter the number of the function you would like to use (1-4): ");
+        printf("(3) Search in historyfor a URL you have shortened\n");
+        printf("(4) Credits\n");
+        printf("(5) Exit the program\n");
+        printf("\nEnter the number of the function you would like to use (1-5): ");
         scanf("%s", input); //receiving as string then converting to int validates value
 		functionNumber = atoi(input);
 
@@ -103,16 +234,29 @@ void DisplayMenu(){
                 fprintf(stderr, "curl_easy_init() failed\n");
                 break;
             case 2:
-                //print list
+                printf("Here is your URL shortening history: --------------------------------------------\n");
                 PrintList(historyList);
+                printf("End of History ------------------------------------------------------------------\n");
                 break;
 
             case 3:
+                printf("\nEnter the string you would like to search.\n");
+                char searchString[256];
+				printf("Your search request: ");
+				scanf("%s", searchString);
+
+				printf("\nYour search results: ----------------------------------------------------------\n");
+				PrintSearch(historyList, searchString);
+				printf("\nEND OF LIST--------------------------------------------------------------------\n");
+
+                break;
+
+            case 4:
                 //credits
                 PrintDeveloperCredits();
                 break;
 
-            case 4:
+            case 5:
                 //exit program
                 cont = 0;
                 break;
